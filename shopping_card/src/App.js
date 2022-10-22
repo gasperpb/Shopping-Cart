b73b065ac217515087833c2362cfe181abe6bbd8
gasperpb <achillespb@hotmail.com>
2022-10-22 09:59:04 -0300

    criado a lista

--- shopping_card/src/App.js
+++ shopping_card/src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Amazon from "./components/Amazon";
+import Cart from "./components/Cart";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [show, setShow] = useState(true);
+  const [cart, setCart] = useState([]);
   return (
     <React.Fragment>
-      <Navbar />
+      <Navbar />j
+      <Amazon />
+      <Cart />
     </React.Fragment>
   );
 }
